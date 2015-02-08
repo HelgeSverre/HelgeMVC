9b764b0249e389d9b19e25aa07127829d372511c
Helge Sverre Hessevik Liseth <email@helgesverre.com>
2015-02-08 22:59:11 +0100

    Corrected error in flash messaging class, misunderstood how static variables worked Flash::$hasMessage changed to Flash::hasMessage()

--- application/libs/Flash.php
+++ application/libs/Flash.php
@@ -7,33 +7,30 @@
  * in HelgeMVC is a session based flash message which will
  * only be displayed once, then deleted
  */
-class Flash
-{
-
-    /**
-     * @var bool if there are any flash messages to display
-     */
-    public static $hasMessage = false;
+class Flash {
 
     /**
      * @param string $message sets a flash message
      */
-    public static function set($message)
-    {
+    public static function set($message) {
         Session::set("flash", $message);
-        self::$hasMessage = true;
     }
 
+
+    /**
+     * @return bool whether or not there are a flash message
+     */
+    public static function hasMessage() {
+        return !is_null(Session::get("flash")) ? true : false;
+    }
 
     /**
      * Gets the flash message and unsets it.
      * @return mixed the flash message string or null
      */
-    public static function get()
-    {
+    public static function get() {
         $tmp = Session::get("flash");
         Session::delete("flash");
-        self::$hasMessage = false;
         return $tmp;
     }
 
